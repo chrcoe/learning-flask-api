3c84f0fa9dd9f5029167bc60342cc633feea5af5
Chris Coe <chrcoe@ieee.org>
2015-10-24 13:12:40 -0400

    initial commit after setting up cookiecutter

---
+++ api/config.py
@@ -0,0 +1,7 @@
+import os
+
+basepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basepath, 'dev.db')
+SECRET_KEY = 'Your Secret Key!!!'
+JWT_AUTH_URL = '/auth/token'
